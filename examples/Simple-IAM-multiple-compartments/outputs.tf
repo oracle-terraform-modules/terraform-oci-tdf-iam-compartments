f48a703a626392a8fbbbaa5b6d58fb54cc81a803
cosmindev <cosmin.tudor82@gmail.com>
2022-03-16 19:02:24 +0200

    fix output issue

--- examples/Simple-IAM-multiple-compartments/outputs.tf
+++ examples/Simple-IAM-multiple-compartments/outputs.tf
@@ -9,7 +9,7 @@
 output "compartments_config" {
   description = "compartments:"
   value = {
-    compartments = module.oci_iam_compartments.compartments_config["compartments"],
+    compartments = module.oci_iam_compartments.compartments_config,
     home_region  = [for i in data.oci_identity_region_subscriptions.this.region_subscriptions : i.region_name if i.is_home_region == true][0]
   }
 }
